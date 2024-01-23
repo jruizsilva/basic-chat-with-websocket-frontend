7e2a4f109994bdd96d9e5774a71d4fb1b7e59c9f
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-22 23:25:02 -0300

    message status removed

--- src/types/chats.d.ts
+++ src/types/chats.d.ts
@@ -2,8 +2,5 @@ interface ChatRoom {
   id: number
   chatName: string
   messages: PrivateMessage[]
-  status: MessageStatus
 }
 type ChatRoomRequest = Omit<ChatRoom, 'id' | 'messages' | 'status'>
-
-type MessageStatus = 'UNREAD' | 'READ'
